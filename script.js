10a994f764652e49fdf1911f4c4f03d3deca3a2b
ashan <indrajee79@gmail.com>
2022-11-09 12:13:11 +0530

    more changes

--- script.js
+++ script.js
@@ -5,17 +5,27 @@ arrowIcons = document.querySelectorAll('.wrapper i');
 let isDragStart = false, prevPageX, prevScrollLeft;
 let firstImgWidth = firstImg.clientWidth + 14; // getting first img width and adding 14 margin value
 
+
+const showHideIcons = () => {
+    // showing and hiding prev/next icon according to carousel scroll left value
+    let scrollWidth = carousel.scrollWidth = carousel.clientWidth // getting max scrollable width
+    arrowIcons[0].style.display = carousel.scrollLeft == 0 ? "none" : "block";
+    arrowIcons[1].style.display = carousel.scrollLeft == scrollWidth ? "none" : "block";
+}
+
 arrowIcons.forEach(icon => {
     icon.addEventListener('click', () => {
+        let firstImgWidth = firstImg.clientWidth //
         // if clicked icon is left, reduce width value from the carousal scroll left alse add to it
         carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
+        setTimeout(() => showHideIcons(), 60) // calling showHideIcons after 60s
     })
 });
 
 const dragStart = (e) => {
     // updating global variables value on mouse down event
     isDragStart = true;
-    prevPageX = e.pageX;
+    prevPageX = e.pageX || e.touched[0].pageX;
     prevScrollLeft = carousel.scrollLeft;
 }
 
@@ -24,8 +34,9 @@ const dragging = (e) => {
     if (!isDragStart) return;
     e.preventDefault();
     carousel.classList.add('dragging')
-    let positionDiff = e.pageX -  prevPageX;
+    let positionDiff = (e.pageX || e.touched[0].pageX) -  prevPageX;
     carousel.scrollLeft = prevScrollLeft - positionDiff;
+    showHideIcons();
 }
 
 const dragStop = () => {
@@ -34,5 +45,11 @@ const dragStop = () => {
 }
 
 carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("touchstart", dragStart);
+
 carousel.addEventListener("mousemove", dragging);
+carousel.addEventListener("touchmove", dragging);
+
 carousel.addEventListener("mouseup", dragStop);
+carousel.addEventListener("mouseleave", dragStop);
+carousel.addEventListener("touchend", dragStop);
